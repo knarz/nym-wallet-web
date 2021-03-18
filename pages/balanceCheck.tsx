4b14dfbf4f88920ee1f39426b96d5b8c8f58ed1f
Jędrzej Stuczyński <jedrzej.stuczynski@gmail.com>
2021-03-18 14:28:00 +0000

    Fixed error on refreshing balance check page

--- pages/balanceCheck.tsx
+++ pages/balanceCheck.tsx
@@ -53,7 +53,9 @@ export default function CheckBalance() {
 
     useEffect(() => {
         const updateBalance = async () => {
-            await getBalance()
+            if (client !== null) {
+                await getBalance()
+            }
         }
         updateBalance()
     }, [])
